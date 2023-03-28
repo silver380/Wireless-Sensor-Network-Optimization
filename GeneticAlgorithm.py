f8a8d16fac6eaae788336a7769797917400bbd75
silver380 <r.rastegari380@outlook.com>
2023-03-28 17:37:27 +0430

    mution demo ready

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -3,8 +3,16 @@ import util
 class GA:
     def __init__(self, n_iter, mut_prob, recomb_prob, tower_construction_cost,
                   tower_maintanance_cost, user_satisfaction_scores):
-        pass
+        self.population_size = 50
+        self.offspring = 50
 
 
     def parent_selection(self):
         pass
+
+    def survival_selection(self):
+        pass
+
+    def is_terminated(self):
+        pass
+
--- chromosome.py
+++ chromosome.py
@@ -1,4 +1,3 @@
-import util
 import random
 
 class Chromosome:
