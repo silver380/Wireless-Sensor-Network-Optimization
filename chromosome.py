056f520997b508bc2f5e6ce9641a8250a4c71629
silver380 <r.rastegari380@outlook.com>
2023-03-31 10:41:38 +0430

    better normalization

--- chromosome.py
+++ chromosome.py
@@ -156,7 +156,7 @@ class Chromosome:
                                                                             - self.pop_sum*self.user_satisfaction_penalty)
         towers_maintanance_cost_norm = 0
         if towers_maintanance_cost != 0:
-            towers_maintanance_cost_norm = towers_maintanance_cost / ((self.max_BW * len(self.towers) * self.tower_maintanance_cost))
+            towers_maintanance_cost_norm = (towers_maintanance_cost - self.min_BW) / ((self.max_BW * self.map_size ** 2 * self.tower_maintanance_cost) - self.min_BW)
         
         towers_constrcution_cost_norm = towers_constrcution_cost / (self.map_size ** 2 * self.tower_construction_cost)
        
