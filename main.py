a0fc7971268a2ff3fe3d0e65027b3fac5866172e
silver380 <r.rastegari380@outlook.com>
2023-04-14 19:41:04 +0430

    0.9 0.9 added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import util
 import numpy as np
+import time
 from evolutionary_algorithm import EvolutionaryAlgorithm as EA
 
 problem_config_file = 'problem_config.txt'
@@ -13,7 +14,7 @@ max_BW = 0
 n_iter = util.n_iter
 population_size = 50
 mut_prob = 0.9
-recomb_prob = 0.1
+recomb_prob = 0.9
 pop_avg = 0
 pop_sum = 0
 histories = []
@@ -31,6 +32,7 @@ if __name__ == "__main__":
    
    
    max_BW = util.calculate_max_BW(map_size, pop_sum,user_satisfaction_levels[-1])
+   start_time = time.time()
    for i in range(10):
       print(f"program number={i+1} / 10, map size= {map_size}, max_BW= {max_BW}, population sum = {pop_sum}, recomb_prob= {recomb_prob}, mut_prob= {mut_prob}")
       ea = EA(n_iter, mut_prob, map_size, blocks_population, recomb_prob, tower_construction_cost,
@@ -43,7 +45,9 @@ if __name__ == "__main__":
          best_ans_price = ans_price
          best_towers = ans_towers
          best_ans_satisfaction = ans_satisfaction
-
+end_time = time.time()
+runtime = end_time - start_time
+print("Runtime: {:.2f} seconds".format(runtime))
 print(f"best found answer has {best_ans} fitness, {best_towers} towers, {best_ans_price} price and {best_ans_satisfaction} user satisfaction")
 histories = np.array(histories)   
 np.savetxt("histories.csv", histories,
